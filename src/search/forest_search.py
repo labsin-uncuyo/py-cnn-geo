0fd280462d26556d54480c92353225a8740f2f0f
Caffaratti Gabriel <sxsoft@gmail.com>
2019-11-23 18:46:03 -0300

    Adding new feature selection style (by vectors)

--- src/search/forest_search.py
+++ src/search/forest_search.py
@@ -1,4 +1,5 @@
 import sys, getopt
+
 sys.path.append("..")
 import gc
 import numpy as np
@@ -11,6 +12,7 @@ from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
 from config import DatasetConfig, RasterParams
 
+
 def main(argv):
     """
     Main function which shows the usage, retrieves the command line parameters and invokes the required functions to do
@@ -22,10 +24,12 @@ def main(argv):
     print('Preparing for random forest parameter search')
     dataset_folder = None
     neighbors = None
+    feature_reduction = None
     finish_earlier = False
+    use_vector_features = False
 
     try:
-        opts, args = getopt.getopt(argv, "hs:n:f", ["dataset_folder=", "neighbors="])
+        opts, args = getopt.getopt(argv, "hs:n:r:vf", ["dataset_folder=", "neighbors=", "feature_reduction="])
     except getopt.GetoptError:
         print('forest_search.py -s <dataset_folder>')
         sys.exit(2)
@@ -37,6 +41,10 @@ def main(argv):
             dataset_folder = arg
         elif opt in ["-n", "--neighbors"]:
             neighbors = int(arg)
+        elif opt in ["-r", "--feature_reduction"]:
+            feature_reduction = int(arg)
+        elif opt in ["-v", "--use_vector"]:
+            use_vector_features = True
         elif opt in ["-f", "--finish_earlier"]:
             finish_earlier = True
     print('Working with dataset folder %s' % dataset_folder)
@@ -45,28 +53,38 @@ def main(argv):
     seed = 7
     np.random.seed(seed)
 
-    parameter_searcher(dataset_folder, neighbors, finish_earlier)
+    if feature_reduction is None:
+        feature_reduction = 1
+
+    parameter_searcher(dataset_folder, neighbors, feature_reduction, use_vector_features, finish_earlier)
 
     sys.exit()
 
 
-def parameter_searcher(dataset_folder, neighbors, finish_earlier):
+def parameter_searcher(dataset_folder, neighbors, feature_reduction, use_vector_features, finish_earlier):
     dataset, dataset_gt, dataset_idxs = prepare_generator_dataset(dataset_folder, neighbors)
+
+    if use_vector_features:
+        center = int(np.floor(neighbors / 2))
+        paths = calculate_feature_paths(neighbors, center, feature_reduction)
+    else:
+        center = None
+        paths = None
 
     # create model
     model = KerasBatchClassifier(build_fn=create_model, verbose=1)
     # define the grid search parameters
-    n_ft = [3,5,'sqrt','auto']
+    n_ft = [3, 5, 8, 'sqrt']
     n_est = [500, 1000, 1500]
-    crit = ['gini']
     min_smpl_spl = [2, 5, 10]
     min_smpl_leaf = [1, 2, 4]
 
-    name_args = ['n_ft', 'n_est', 'crit', 'min_smpl_spl', 'min_smpl_leaf']
-    param_grid = dict(n_ft=n_ft, n_est=n_est, crit=crit, min_smpl_spl=min_smpl_spl, min_smpl_leaf=min_smpl_leaf)
+    name_args = ['n_ft', 'n_est', 'min_smpl_spl', 'min_smpl_leaf']
+    param_grid = dict(n_ft=n_ft, n_est=n_est, min_smpl_spl=min_smpl_spl, min_smpl_leaf=min_smpl_leaf)
     grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1, error_score='raise', verbose=1, cv=3)
 
-    grid_result = grid.fit(dataset_idxs, dataset_idxs, dataset=dataset, dataset_gt=dataset_gt, name_args=name_args, neighbors=neighbors, finish_earlier=finish_earlier)
+    grid_result = grid.fit(dataset_idxs, dataset_idxs, dataset=dataset, dataset_gt=dataset_gt, name_args=name_args,
+                           neighbors=neighbors, paths=paths, center=center, finish_earlier=finish_earlier)
 
     # summarize results
     print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
@@ -76,14 +94,14 @@ def parameter_searcher(dataset_folder, neighbors, finish_earlier):
     params = grid_result.cv_results_['params']
     for mean, stdev, param in zip(means, stds, params):
         print("%f (%f) with: %r" % (mean, stdev, param))
-    #print(model.summary())
+    # print(model.summary())
 
 
 def create_model(n_ft='auto', n_est=5000, crit='gini', min_smpl_spl=2, min_smpl_leaf=1):
-    #print('---> Create model parameters: features %s, estimators %s, criterion %s, sample split %s, sample leaf %s' % (str(n_ft), str(n_est), str(crit), str(min_smpl_spl), str(min_smpl_leaf)))
+    # print('---> Create model parameters: features %s, estimators %s, criterion %s, sample split %s, sample leaf %s' % (str(n_ft), str(n_est), str(crit), str(min_smpl_spl), str(min_smpl_leaf)))
 
     model = RandomForestClassifier(n_estimators=50, max_features=n_ft, criterion=crit, min_samples_split=min_smpl_spl,
-                                 min_samples_leaf=min_smpl_leaf, warm_start=True, verbose=0, n_jobs=-1)
+                                   min_samples_leaf=min_smpl_leaf, warm_start=True, verbose=0, n_jobs=-1)
     return model
 
 
@@ -147,6 +165,50 @@ def prepare_generator_dataset(dataset_folder, padding):
 
     return bigdata, bigdata_gt, bigdata_idx_mix
 
+
+def path_values(paths, path_i, center, traslation_x, traslation_y):
+    for i in range(1, center + 1):
+        r = np.around(np.around((i * traslation_x) + center, decimals=1))
+        h = np.around(np.around((i * traslation_y) + center, decimals=1))
+        paths[path_i, i - 1] = [r, h]
+
+
+def calculate_feature_paths(neighbors, center, step):
+    paths = np.zeros(shape=(int(center * 2 * 4 / step), center, 2), dtype=np.int8)
+
+    # top of the matrix
+    for path_i, h in enumerate(range(-1 * center, center, step)):
+        v = -1 * center
+        traslation_x = h / center
+        traslation_y = v / center
+        path_values(paths, path_i, center, traslation_x, traslation_y)
+
+    # right of the matrix
+    path_current = path_i + 1
+    for path_i, v in enumerate(range(-1 * center, center, step)):
+        h = 1 * center
+        traslation_x = h / center
+        traslation_y = v / center
+        path_values(paths, path_current + path_i, center, traslation_x, traslation_y)
+
+    # bottom of the matrix
+    path_current += path_i + 1
+    for path_i, h in enumerate(range(center, -1 * center, -1*step)):
+        v = 1 * center
+        traslation_x = h / center
+        traslation_y = v / center
+        path_values(paths, path_current + path_i, center, traslation_x, traslation_y)
+
+    # right of the matrix
+    path_current += path_i + 1
+    for path_i, v in enumerate(range(center, -1 * center, -1*step)):
+        h = -1 * center
+        traslation_x = h / center
+        traslation_y = v / center
+        path_values(paths, path_current + path_i, center, traslation_x, traslation_y)
+
+    paths = np.apply_along_axis(np.append, 1, paths, center)
+    return paths
 
 
 main(sys.argv[1:])
--- src/search/random_forest_keras_batch_classifier.py
+++ src/search/random_forest_keras_batch_classifier.py
@@ -19,13 +19,17 @@ class KerasBatchClassifier(KerasClassifier):
 
     def __init__(self, build_fn=None, verbose=0):
         super().__init__(build_fn=build_fn, verbose=verbose)
-        #self.dataset = None
-        #self.dataset_gt = None
+        # self.dataset = None
+        # self.dataset_gt = None
         self.dir_name = None
         self.name = None
         self.store_path = None
         self.neighbors = None
         self.feature_groups = None
+        self.paths = None
+        self.center = None
+        self.feature_extraction_fn = None
+        self.n_features = None
         self.date_and_time = None
         self.final_estimators = None
 
@@ -38,6 +42,8 @@ class KerasBatchClassifier(KerasClassifier):
         dataset_gt = kwargs['dataset_gt']
         naming_args = kwargs['name_args']
         self.neighbors = kwargs['neighbors']
+        self.paths = kwargs['paths']
+        self.center = kwargs['center']
         # taken from keras.wrappers.scikit_learn.KerasClassifier.fit ###################################################
         if self.build_fn is None:
             self.model = self.__call__(**self.filter_sk_params(self.__call__))
@@ -46,8 +52,8 @@ class KerasBatchClassifier(KerasClassifier):
         else:
             self.model = self.build_fn(**self.filter_sk_params(self.build_fn))
 
-        #loss_name = self.model.loss
-        #if hasattr(loss_name, '__name__'):
+        # loss_name = self.model.loss
+        # if hasattr(loss_name, '__name__'):
         #    loss_name = loss_name.__name__
 
         '''if loss_name == 'binary_crossentropy' and len(y.shape) != 2:
@@ -57,9 +63,9 @@ class KerasBatchClassifier(KerasClassifier):
         ################################################################################################################
         self.dir_name = self.build_name(naming_args, self.sk_params)
         self.date_and_time = '{date:%Y%m%d_%H%M%S}'.format(date=datetime.datetime.now())
-        self.name = self.date_and_time + '.' + self.dir_name
+        self.name = self.date_and_time + '.' + self.dir_name + ('-vec' if self.paths is not None else '-cir')
 
-        self.store_path = join("../storage/search/", self.dir_name)
+        self.store_path = join("../storage/rf-search/vec/", self.dir_name)
         if not exists(self.store_path):
             makedirs(self.store_path)
 
@@ -77,31 +83,34 @@ class KerasBatchClassifier(KerasClassifier):
         steps = int(n_estimators / estimator_step_size) + 1
         batch_size = int(X_train.shape[0] / steps) + 1
 
-        print('Starting training phase...')
+        if self.paths is not None:
+            self.feature_extraction_fn = get_item_features_vector
+            self.n_features = (self.paths.shape[0] + 1) * self.paths.shape[1]
+        else:
+            self.feature_extraction_fn = get_item_features
+            self.n_features = dataset.shape[1] * self.feature_groups
 
-        try:
-            stdscr = curses.initscr()
-            curses.noecho()
-            curses.cbreak()
-        except curses.error as e:
-            print('Expected curses error... just ignore it')
+        print('Starting training phase...')
 
         self.final_estimators = self.model.n_estimators
 
+        print('Train progress: ', end='')
+
         for i in range(steps):
-            self.progress_bar(steps, i + 1, 'Train', stdscr)
+            print('{0}/{1} - '.format(i + 1, steps), end='')
+            #self.progress_bar(steps, i + 1, 'Train', stdscr)
             start = (i) * batch_size
             end = (i + 1) * batch_size
             end = end if end < X_train.shape[0] else X_train.shape[0]
 
-            X_preprocessed_train_bag = np.zeros(shape=(end - start, dataset.shape[1] * self.feature_groups),
+            X_preprocessed_train_bag = np.zeros(shape=(end - start, self.n_features),
                                                 dtype=np.float32)
             Y_preprocessed_train_bag = np.zeros(shape=(end - start), dtype=np.uint8)
             pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
             X_preprocessed_train_bag = np.array(
                 pool.map(
-                    partial(get_item_features, feature_groups=self.feature_groups, neighbors=kwargs['neighbors']),
-                    X_train[start:end, :]))
+                    partial(self.feature_extraction_fn, feature_groups=self.feature_groups, neighbors=self.neighbors,
+                            paths=self.paths, center=self.center), X_train[start:end, :]))
             Y_preprocessed_train_bag = np.array(pool.map(get_item_gt, X_train[start:end, :]))
             pool.close()
             pool.join()
@@ -132,14 +141,7 @@ class KerasBatchClassifier(KerasClassifier):
                     self.model.set_params(n_estimators=estimators_left)
                 self.final_estimators += self.model.n_estimators
 
-        try:
-            curses.echo()
-            curses.nocbreak()
-            curses.endwin()
-        except curses.error as e:
-            print('Expected curses error... just ignore it')
-        finally:
-            pass
+        print('Finished!\n')
 
         X_preprocessed_train_bag = None
         Y_preprocessed_train_bag = None
@@ -150,32 +152,29 @@ class KerasBatchClassifier(KerasClassifier):
 
         val_batch_size = int(X_val.shape[0] / steps) + 1
 
-        try:
-            stdscr = curses.initscr()
-            curses.noecho()
-            curses.cbreak()
-        except curses.error as e:
-            print('Expected curses error... just ignore it')
-
-        random_trees_files = [f for f in listdir(self.store_path) if not isfile(join(self.dir_name, f)) and f.startswith(self.date_and_time) and f.endswith('.pkl')]
+        random_trees_files = [f for f in listdir(self.store_path) if
+                              not isfile(join(self.dir_name, f)) and f.startswith(self.date_and_time) and f.endswith(
+                                  '.pkl')]
         random_trees_files = natsorted(random_trees_files, key=lambda y: y.lower())
 
         values_votes = np.zeros(shape=(X_val.shape[0], 2), dtype=np.float32)
-        expected_val = np.zeros(shape=(X_val.shape[0], ), dtype=np.uint8)
+        expected_val = np.zeros(shape=(X_val.shape[0],), dtype=np.uint8)
+
+        print('Validation progress: ', end='')
 
         for i in range(steps):
-            self.progress_bar(steps, i + 1, 'Validation', stdscr)
+            print('{0}/{1} - '.format(i + 1, steps), end='')
             start = (i) * val_batch_size
             end = (i + 1) * val_batch_size
             end = end if end < X_val.shape[0] else X_val.shape[0]
 
-            X_partial_preprocessed_val_bag = np.zeros(shape=(end - start, dataset.shape[1] * self.feature_groups),
-                                                      dtype=np.float32)
-            #expected_val[start:end] = np.zeros(shape=(end - start), dtype=np.uint8)
+            X_partial_preprocessed_val_bag = np.zeros(shape=(end - start, self.n_features), dtype=np.float32)
+            # expected_val[start:end] = np.zeros(shape=(end - start), dtype=np.uint8)
             pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
             X_partial_preprocessed_val_bag = np.array(
-                pool.map(partial(get_item_features, feature_groups=self.feature_groups, neighbors=self.neighbors),
-                         X_val[start:end, :]))
+                pool.map(
+                    partial(self.feature_extraction_fn, feature_groups=self.feature_groups, neighbors=self.neighbors,
+                            paths=self.paths, center=self.center), X_val[start:end, :]))
             expected_val[start:end] = np.array(pool.map(get_item_gt, X_val[start:end, :]))
             pool.close()
             pool.join()
@@ -185,14 +184,19 @@ class KerasBatchClassifier(KerasClassifier):
                 self.model = joblib.load(rf_full_filename)
 
                 if file_i == 0:
-                    batch_votes = np.multiply(self.model.predict_proba(X_partial_preprocessed_val_bag), self.model.n_estimators)
+                    batch_votes = np.multiply(self.model.predict_proba(X_partial_preprocessed_val_bag),
+                                              self.model.n_estimators)
                 else:
-                    batch_votes = np.add(batch_votes, np.multiply(self.model.predict_proba(X_partial_preprocessed_val_bag), self.model.n_estimators))
+                    batch_votes = np.add(batch_votes,
+                                         np.multiply(self.model.predict_proba(X_partial_preprocessed_val_bag),
+                                                     self.model.n_estimators))
 
                 self.model = None
                 gc.collect()
 
             values_votes[start:end] = batch_votes
+
+        print('Finished!')
 
         predicted_val = np.divide(values_votes, self.final_estimators)
         X_partial_preprocessed_val_bag = None
@@ -203,16 +207,7 @@ class KerasBatchClassifier(KerasClassifier):
 
         val_acc = accuracy_score(expected_val, predicted_val)
 
-        try:
-            curses.echo()
-            curses.nocbreak()
-            curses.endwin()
-        except curses.error as e:
-            print('Expected curses error... just ignore it')
-        finally:
-            pass
-
-        print('Validation score: {val_acc:.4f}'.format(val_acc=val_acc))
+        print('Validation score: {val_acc:.4f}\n'.format(val_acc=val_acc))
 
         print('Storing value accuracy...')
         open(join(self.store_path, self.name + '-{val_acc:.4f}'.format(val_acc=val_acc) + '.txt'), O_CREAT)
@@ -238,27 +233,21 @@ class KerasBatchClassifier(KerasClassifier):
 
         print('Starting testing phase...')
 
-        try:
-            stdscr = curses.initscr()
-            curses.noecho()
-            curses.cbreak()
-        except curses.error as e:
-            print('Expected curses error... just ignore it')
-
         values_votes = np.zeros(shape=(X.shape[0], 2), dtype=np.float32)
 
+        print('Test progress: ', end='')
         for i in range(steps):
-            self.progress_bar(steps, i + 1, 'Test', stdscr)
+            print('{0}/{1} - '.format(i + 1, steps), end='')
             start = (i) * batch_size
             end = (i + 1) * batch_size
             end = end if end < X.shape[0] else X.shape[0]
 
-            X_partial_preprocessed_test_bag = np.zeros(shape=(end - start, dataset.shape[1] * self.feature_groups),
-                                                       dtype=np.float32)
+            X_partial_preprocessed_test_bag = np.zeros(shape=(end - start, self.n_features), dtype=np.float32)
             pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
             X_partial_preprocessed_test_bag = np.array(
-                pool.map(partial(get_item_features, feature_groups=self.feature_groups, neighbors=self.neighbors),
-                         X[start:end, :]))
+                pool.map(
+                    partial(self.feature_extraction_fn, feature_groups=self.feature_groups, neighbors=self.neighbors,
+                            paths=self.paths, center=self.center), X[start:end, :]))
             expected_test[start:end] = np.array(pool.map(get_item_gt, X[start:end, :]))
             pool.close()
             pool.join()
@@ -268,14 +257,19 @@ class KerasBatchClassifier(KerasClassifier):
                 self.model = joblib.load(rf_full_filename)
 
                 if file_i == 0:
-                    batch_votes = np.multiply(self.model.predict_proba(X_partial_preprocessed_test_bag), self.model.n_estimators)
+                    batch_votes = np.multiply(self.model.predict_proba(X_partial_preprocessed_test_bag),
+                                              self.model.n_estimators)
                 else:
-                    batch_votes = np.add(batch_votes, np.multiply(self.model.predict_proba(X_partial_preprocessed_test_bag), self.model.n_estimators))
+                    batch_votes = np.add(batch_votes,
+                                         np.multiply(self.model.predict_proba(X_partial_preprocessed_test_bag),
+                                                     self.model.n_estimators))
 
                 self.model = None
                 gc.collect()
 
             values_votes[start:end] = batch_votes
+
+        print('Finished!\n')
 
         predicted_test = np.divide(values_votes, self.final_estimators)
         X_partial_preprocessed_test_bag = None
@@ -283,16 +277,6 @@ class KerasBatchClassifier(KerasClassifier):
         values_votes = None
         gc.collect()
         predicted_test = np.argmax(predicted_test, axis=1)
-
-        try:
-            curses.echo()
-            curses.nocbreak()
-            curses.endwin()
-        except curses.error as e:
-            print('Expected curses error... just ignore it')
-        finally:
-            pass
-
 
         print('Calculating reports and metrics...')
 
@@ -312,7 +296,6 @@ class KerasBatchClassifier(KerasClassifier):
 
         return test_acc
 
-
     def divide_indexes(self, indexes_file):
         split_1 = int(SamplesConfig.TEST_PERCENTAGE * indexes_file.shape[0])
 
@@ -320,7 +303,6 @@ class KerasBatchClassifier(KerasClassifier):
         validation_idxs = indexes_file[:split_1]
 
         return train_idxs, validation_idxs
-
 
     def build_name(self, naming_args, sk_params):
 
@@ -338,8 +320,8 @@ class KerasBatchClassifier(KerasClassifier):
         return name
 
     def print_confusion_matrix(self, y_true, y_pred, classes,
-                              normalize=False,
-                              title=None):
+                               normalize=False,
+                               title=None):
         """
         This function prints the confusion matrix.
         Normalization can be applied by setting `normalize=True`.
@@ -381,7 +363,11 @@ class KerasBatchClassifier(KerasClassifier):
     def history(self):
         return self.__history
 
-def get_item_features(item, feature_groups=None, neighbors=None):
+
+def get_item_features(item, feature_groups=None, neighbors=None, paths=None, center=None):
+    # Paths and center are not used in this function para we keep the parameter to maintain the same function definition
+    # as get_item_features_vector and avoid an if statement being executed billion of times
+
     data_patch = dataset[item[0], :, item[1]: item[1] + neighbors, item[2]: item[2] + neighbors]
 
     np_feat = np.zeros(shape=(data_patch.shape[0] * feature_groups,), dtype=np.float32)
@@ -403,5 +389,21 @@ def get_item_features(item, feature_groups=None, neighbors=None):
 
     return np_feat
 
+
 def get_item_gt(item):
     return dataset_gt[item[0], item[1], item[2]]
+
+
+def get_item_features_vector(item, feature_groups=None, neighbors=None, paths=None, center=None):
+    # Feature groups are not used in this function para we keep the parameter to maintain the same function definition
+    # as get_item_features and avoid an if statement being executed billion of times
+
+    data_patch = dataset[item[0], :, item[1]: item[1] + neighbors, item[2]: item[2] + neighbors]
+    data_feat = np.array([np.mean([data_patch[:, g[0], g[1]] for g in f], axis=0) for f in paths], dtype=np.float32)
+
+    data_center = data_patch[:, center, center]
+    data_center = data_center.reshape(1, data_center.shape[0])
+    data_feat = np.append(data_feat, data_center, axis=0)
+
+    data_feat = data_feat.reshape(data_feat.shape[0] * data_feat.shape[1])
+    return data_feat
